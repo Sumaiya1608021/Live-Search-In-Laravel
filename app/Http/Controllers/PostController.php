63b7cdf6864889e7ee1ab46820d490d97445cd38
Sumaiya <sumaiya.16.ete.cuet@gmail.com>
2024-01-08 10:04:09 +0600

    Live search in laravel

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+class PostController extends Controller
+{
+ 
+  public function search(Request $request){
+
+    if($request->ajax()){
+
+        $data=Post::where('id','like','%'.$request->search.'%')
+        ->orwhere('title','like','%'.$request->search.'%')
+        ->orwhere('description','like','%'.$request->search.'%')->get();
+
+
+        $output='';
+    if(count($data)>0){
+
+         $output ='
+            <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+            </tr>
+            </thead>
+            <tbody>';
+
+                foreach($data as $row){
+                    $output .='
+                    <tr>
+                    <th scope="row">'.$row->id.'</th>
+                    <td>'.$row->title.'</td>
+                    <td>'.$row->description.'</td>
+                    </tr>
+                    ';
+                }
+
+
+
+         $output .= '
+             </tbody>
+            </table>';
+
+
+
+    }
+    else{
+
+        $output .='No results';
+
+    }
+
+    return $output;
+
+    }
+
+
+
+
+  }
+
+}
